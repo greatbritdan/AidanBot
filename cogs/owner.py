a8b522ec6509ab33572c83df22ac4fef48c19441
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2021-08-23 13:12:18 +0100

    small tweaks

--- cogs/owner.py
+++ cogs/owner.py
@@ -5,11 +5,62 @@ import io
 import textwrap
 from traceback import format_exception
 
-from functions import getEmbed
+import asyncio
+
+from functions import getEmbed, clear_command_type
 
 class OwnerCog(commands.Cog):
 	def __init__(self, client):
 		self.client = client
+
+	### COGS ###
+
+	@commands.command()
+	@commands.is_owner()
+	async def load(self, ctx, extension):
+		self.client.load_extension(f'cogs.{extension}')
+		await ctx.send('```{} loaded!```'.format(extension))
+
+	@commands.command()
+	@commands.is_owner()
+	async def unload(self, ctx, extension):
+		if extension == "owner":
+			await ctx.send('```owner can not be unloaded, it holds the $load, $unload and $reload function!```')
+			return
+			
+		self.client.unload_extension(f'cogs.{extension}')
+		clear_command_type(extension)
+		await ctx.send('```{} unloaded!```'.format(extension))
+
+	@commands.command()
+	@commands.is_owner()
+	async def forceunload(self, ctx, extension):
+		MSG = await ctx.send('```are you sure you want to unload {}?```'.format(extension))
+		await MSG.add_reaction("✅")
+		await MSG.add_reaction("❎")
+
+		def check(reaction, user):
+			return (user == ctx.author and reaction.message.id == MSG.id and reaction.emoji == "✅" or reaction.emoji == "❎")
+
+		try:
+			reaction, user = await self.client.wait_for("reaction_add", timeout=10, check=check)
+			if reaction.emoji == "✅":
+				self.client.unload_extension(f'cogs.{extension}')
+				clear_command_type(extension)
+				await ctx.send('```{} unloaded!```'.format(extension))
+			else:
+				await ctx.send('```No action was taken.```')
+
+		except asyncio.TimeoutError:
+			await ctx.send('```Timeout```')
+
+	@commands.command()
+	@commands.is_owner()
+	async def reload(self, ctx, extension):
+		self.client.unload_extension(f'cogs.{extension}')
+		clear_command_type(extension)
+		self.client.load_extension(f'cogs.{extension}')
+		await ctx.send('```{} reloaded!```'.format(extension))
 
 	@commands.command()
 	@commands.is_owner()
