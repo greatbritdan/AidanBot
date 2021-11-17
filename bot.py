95926e7faa6be1e40b596c4dfbadae1f49eeb9e5
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2021-11-17 14:13:37 +0000

    V1.1

--- bot.py
+++ bot.py
@@ -1,21 +1,9 @@
 import os
-import discord
-from discord.ext import commands
+from aidanbot import AidanBot
 
-from functions import get_prefix
+def main():
+	client = AidanBot("$")
+	# client.run(os.getenv("DISCORD_TOKEN"))
 
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix=get_prefix(), case_insensitive=True, help_command=None, intents=intents, allowed_mentions=discord.AllowedMentions(everyone=False, roles=False))
-
-@client.event
-async def on_ready():
-	print(f'Logged in: {client.user}')
-
-	await client.change_presence(activity=discord.Activity(name=f'use {get_prefix()}help for help.',type=discord.ActivityType.competing))
-	
-for filename in os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
-		print('{} Loaded!'.format(filename[:-3]))
-
-client.run(os.getenv("DISCORD_TOKEN"))
+if __name__ == '__main__':
+    main()
