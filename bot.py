96ccbe0ed60e1a69dda293f3fc2d45e18275b3f3
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2021-11-21 19:00:41 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ from aidanbot import AidanBot
 
 def main():
 	client = AidanBot()
-	client.run("ODYxNTcxMjkwMTMyNjQzODUw.YOLuyg.lJFpM_ORQSQt1tvVOkHFUinlzcI")
-	# client.run(os.getenv("DISCORD_TOKEN"))
+	client.run(os.getenv("DISCORD_TOKEN"))
 
 if __name__ == '__main__':
     main()
