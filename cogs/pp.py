61285473e31eebcc88d282617e6928df7b1a73f8
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2022-01-15 16:37:25 +0000

    removed duplicate code

--- cogs/pp.py
+++ cogs/pp.py
@@ -43,21 +43,5 @@ class PPCog(commands.Cog):
 		emoji = await guild.create_custom_emoji(name=name, image=image)
 		await ctx.send(emoji)
 
-	@commands.command(description="just for one thing so yeah...")
-	@commands.check(is_pipon_palace)
-	@commands.cooldown(1, 10)
-	async def emoteify(self, ctx, name, addtomain:bool=False):
-		if len(ctx.message.attachments) < 1:
-			await ComError(ctx, self.client, "Needs attachment.")
-			return
-
-		if addtomain and ctx.author.id == self.client.owner_id:
-			guild = ctx.guild
-		else:
-			guild = get(self.client.guilds, id=879063875469860874)
-		image = await ctx.message.attachments[0].read()
-		emoji = await guild.create_custom_emoji(name=name, image=image)
-		await ctx.send(emoji)
-
 def setup(client):
 	client.add_cog(PPCog(client))
