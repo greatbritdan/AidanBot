d6c6dfb628a4d2ec952cf55d82322bc332441bb0
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2022-10-05 23:10:54 +0100

    Best UwU (Also rip Raw)

--- cogs/message.py
+++ cogs/message.py
@@ -8,17 +8,43 @@ class MessageCog(discord.Cog):
 	def __init__(self, client):
 		self.client = client
 
-	@message_command(name="Raw")
-	async def raw(self, ctx, message):
-		msg = message.clean_content.replace("`","'")
-		await ctx.respond(f"Raw text from this message:```{msg}```", ephemeral=True)
-	
 	@message_command(name="UwU")
-	async def uwuify(self, ctx, message):
-		endings = ["UwU", "OwO", ":3", "X3"]
-		msg = message.clean_content.replace("l","w").replace("r","w")
-		msg = "*" + msg + " " + choice(endings) + "*"
-		await ctx.respond(msg)
+	async def uwuify(self, ctx:AC, message:discord.Message):
+		endings = [";;w;;", ";w;", "UwU", "OwO", ":3", "X3", "^_^", "\\* *sweats* *", "\\* *screams* *", "\\* *huggles tightly* *"]
+
+		# start with owour message
+		msg = message.clean_content
+
+		# add some uwunique words
+		repwords = {
+			"love":"wuv", "cherish":"chwish",
+			"Love":"Wuv", "Cherish":"Chwish",
+			"LOVE":"WUV", "CHERISH":"CHWISH"
+		}
+		for repword in repwords:
+			msg = replaceWord(msg,repword,repwords[repword])
+
+		# add s-some dashes to make them s-seem nervowous
+		mwords = msg.split()
+		newmwords = []
+		for i, v in enumerate(mwords):
+			if randint(1,12) == 12:
+				newmwords.append(v[0] + "-" + v)
+			else:
+				newmwords.append(v)
+		msg = " ".join(newmwords)
+
+		# make them sowounds wike a child
+		msg = msg.replace("l","w").replace("r","w").replace("L","W").replace("R","W")
+		msg = "*" + msg + "* " + choice(endings)
+
+		# print(msg) # dowone
+		await ctx.defer(ephemeral=True)
+		try:
+			await self.client.sendWebhook(ctx.channel, message.author, msg, [], " (fake)")
+			await ctx.respond("UwUified!", ephemeral=True)
+		except:
+			await ctx.respond("Error! Try again later!", ephemeral=True)
 
 def setup(client):
 	client.add_cog(MessageCog(client))
