fd73a8c35b883b797231a518b9b8c4d3269aa3d4
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2022-11-22 15:23:07 +0000

    dwadawdaw

--- cogs/core.py
+++ cogs/core.py
@@ -222,7 +222,7 @@ class CoreCog(CM.Cog):
 					emb.add_field(name=field3[0], value=field3[1], inline=False)
 
 			await itr.channel.send(embed=emb)
-			await itr.response.send_message("Embeded!", ephemeral=True)
+			await itr.edit_original_response("Embeded!", ephemeral=True)
 		except ValueError:
 			return await itr.response.send_message("Embed was too boog.", ephemeral=True)
 
