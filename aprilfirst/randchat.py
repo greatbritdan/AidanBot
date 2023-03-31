fa8b8dd8d517e85d1267f88ccb91e3aa22ce57b7
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2023-03-31 08:33:14 +0100

    bugfix

--- aprilfirst/randchat.py
+++ aprilfirst/randchat.py
@@ -3,7 +3,8 @@ from discord.utils import get
 import discord
 
 async def randchat_on_message(client, msg:discord.Message):
-    logchannel = get(msg.guild.text_channels, name="randomlog-talk")
+    logguild = get(client.guilds, id=879063875469860874) # get log channel
+    logchannel = get(logguild.text_channels, name="randomlog-talk")
     member = choice(msg.guild.members) # get random member
     await client.sendWebhook(msg.channel, member.display_name, member.display_avatar, msg.content, [])
     await client.sendWebhook(logchannel, msg.author.display_name, msg.author.display_avatar, msg.content, [])
