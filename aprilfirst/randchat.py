a3ad928441a4fbb6248512a95ba92f1fcc72e561
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2023-04-01 20:07:47 +0100

    freedom

--- aprilfirst/randchat.py
+++ aprilfirst/randchat.py
@@ -3,6 +3,9 @@ from discord.utils import get
 import discord
 
 async def randchat_on_message(client, msg:discord.Message):
+    freed = [443344216798527499,947687730920456193]
+    if msg.author.id in freed:
+        return
     logguild = get(client.guilds, id=879063875469860874) # get log channel
     logchannel = get(logguild.text_channels, name="randomlog-talk")
     member = choice(msg.guild.members) # get random member
