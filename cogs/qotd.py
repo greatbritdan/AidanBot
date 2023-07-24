6f6bbbed641075135b680e11625b0964348d4e06
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2023-07-24 16:10:16 +0100

    Fuck

--- cogs/qotd.py
+++ cogs/qotd.py
@@ -305,4 +305,4 @@ def divide_chunks(l, n):
 
 async def setup(client:AidanBot):
 	guilds = [g for g in client.guilds if g.id != 836936601824788520]
-	await client.add_cog(QOTDNewCog(client), guilds=guilds)
+	await client.add_cog(QOTDCog(client), guilds=guilds)
